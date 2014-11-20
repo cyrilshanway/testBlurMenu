928e8d20f1ebf39a276ba84789dc58ae22443c8d
cyrilshanway <cyrilshanway@gmail.com>
2014-11-20 17:29:52 +0800

    Initial Commit

---
+++ testBlurMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testBlurMenu
+//
+//  Created by Cyrilshanway on 2014/11/20.
+//  Copyright (c) 2014年 Cyrilshanway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testBlurMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testBlurMenu
+//
+//  Created by Cyrilshanway on 2014/11/20.
+//  Copyright (c) 2014年 Cyrilshanway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
